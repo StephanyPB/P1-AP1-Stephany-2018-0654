dbe4f207d1b2e4073a1680b7732848dec49f4228
Stephany Paulino Balbi <stephany_paulino1@ucne.edu.do>
2021-10-04 20:25:45 -0400

    Agregando migracion, DataBase con copy always

---
+++ Migrations/20211005001840_MigracionAportes.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace P1_AP1_Stephany_2018_0654.Migrations
+{
+    public partial class MigracionAportes : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Aportes",
+                columns: table => new
+                {
+                    aporteID = table.Column<int>(type: "INTEGER", nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true),
+                    fecha = table.Column<DateTime>(type: "TEXT", nullable: false),
+                    persona = table.Column<string>(type: "TEXT", nullable: true),
+                    concepto = table.Column<string>(type: "TEXT", nullable: true),
+                    monto = table.Column<float>(type: "REAL", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Aportes", x => x.aporteID);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Aportes");
+        }
+    }
+}
