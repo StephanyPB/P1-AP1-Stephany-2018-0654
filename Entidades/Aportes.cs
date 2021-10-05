7b04a4c2e590849860dec3b6f8fef724614f39ac
Stephany Paulino Balbi <stephany_paulino1@ucne.edu.do>
2021-10-04 20:10:59 -0400

    Creando la entidad aportes con sus propiedades

---
+++ Entidades/Aportes.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace P1_AP1_Stephany_2018_0654.Entidades
+{
+    public class Aportes
+    {
+        [Key]
+        public int aporteID { get; set; }
+        public DateTime fecha { get; set; }
+        public String persona { get; set; }
+        public String concepto { get; set; }
+        public float monto { get; set; }
+
+        public Aportes()
+        {
+            aporteID = 0;
+            fecha = DateTime.Now;
+            persona = string.Empty;
+            concepto = string.Empty;
+            monto = 0;
+        }
+    }
+}
