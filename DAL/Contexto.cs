97542b53fe264abee09da2aeb0bc120fefe1481b
Stephany Paulino Balbi <stephany_paulino1@ucne.edu.do>
2021-10-04 20:16:50 -0400

    Creando el contexto

---
+++ DAL/Contexto.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.EntityFrameworkCore;
+using P1_AP1_Stephany_2018_0654.Entidades;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace P1_AP1_Stephany_2018_0654.DAL
+{
+    public class Contexto : DbContext
+    {
+        public DbSet<Aportes> Aportes { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlite(@"data source = data\aportescontrol.db");
+        }
+    }
+}
